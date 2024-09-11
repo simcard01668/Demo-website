99b2c5c754c9205d2d15ee6e24606dcb71f3e002
Simcard01668 <howingyam0350@gmail.com>
2024-09-11 17:44:01 +0800

    new commit showcase website

---
+++ src/components/Contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Section = styled.div`
+height: 100vh;
+background-color: #de83d2;
+scroll-snap-align: center;
+`
+
+const Contact = () => {
+  return (
+    <Section>Contact</Section>
+  )
+}
+
+export default Contact
---
+++ src/components/Who.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Section = styled.div`
+height: 100vh;
+background-color: #7cd0ce;
+scroll-snap-align: center;`
+
+const Who = () => {
+  return (
+    <Section>Who</Section>
+  )
+}
+
+export default Who
