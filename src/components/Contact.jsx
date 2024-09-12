05a796aaab533190ae7fc2b13c5fc0542a268862
IsaacYam <howingyam0350@gmail.com>
2024-09-13 02:13:51 +0800

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,15 +1,80 @@
-import React from 'react'
+
 import styled from 'styled-components'
 
 const Section = styled.div`
 height: 100vh;
-
 scroll-snap-align: center;
 `
 
+const Container = styled.div`
+width: 100%;
+height: 100%;
+display: flex;
+justify-content: space-between;
+gap: 50px;
+`
+
+const Left = styled.div`
+flex: 1;
+display: flex;
+align-items: center;
+justify-content: flex-end;
+`
+const Form = styled.form`
+display: flex;
+flex-direction: column;
+width: 500px;
+gap: 25px;`
+
+const Title = styled.h1`
+font-weight: 200;
+`
+
+const Input = styled.input`
+padding: 20px;
+background-color: #e7e4e4;
+border: none;
+border-radius: 5px;
+`
+
+const Textarea = styled.textarea`
+padding: 20px;
+background-color: #e7e4e4;
+border: none;
+border-radius: 5px;`
+
+const Button = styled.button`
+background-color: #da4ea2;
+color: white;
+outline: none;
+border: none;
+padding: 20px;
+border-radius: 5px;
+cursor: pointer;`
+
+const Right = styled.div`
+flex: 1;
+`
+
+
 const Contact = () => {
   return (
-    <Section>Contact</Section>
+    <Section>
+      <Container>
+        <Left>
+          <Form>
+            <Title>Contact Us</Title>
+            <Input type="text" placeholder="Name" />
+            <Input type="text" placeholder="Email" />
+            <Textarea placeholder="Write your Message" rows={10}></Textarea>
+            <Button>Send</Button>
+          </Form>
+        </Left>
+        <Right>
+
+        </Right>
+      </Container>
+    </Section>
   )
 }
 
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,14 +1,73 @@
-import React from 'react'
 import styled from 'styled-components'
+import './works.css'
 
+const data = [
+  "Web Design",
+  "Development",
+  "Illustration",
+  "Product Design",
+  "Social Media"
+]
 const Section = styled.div`
 height: 100vh;
+scroll-snap-align: center;
+display: flex;
+justify-content: center;
+`
 
-scroll-snap-align: center;`
+const Container = styled.div`
+width: 1400px;
+display: flex;
+justify-content: space-between;
+`
+
+const Left = styled.div`
+flex: 1;
+display: flex;
+align-items: center;
+`
+
+const List = styled.ul`
+flex-direction: column;
+display: flex;
+gap: 20px;
+list-style: none;
+`
+
+const ListItem = styled.li`
+  font-size: 100px;
+  font-weight: bold;
+  cursor: pointer;
+  color: transparent;
+  -webkit-text-stroke: 1px white;
+  position: relative;
+`
+
+
+
+const Right = styled.div`
+flex: 1;`
+
 
 const Works = () => {
   return (
-    <Section>Works</Section>
+    <Section>
+      <Container>
+        <Left>
+          <List>
+            {data.map((item) => {
+              return (
+                <ListItem key={item}>{item}</ListItem>
+              )
+            })}
+          </List>
+        </Left>
+
+        <Right>
+
+        </Right>
+      </Container>
+    </Section>
   )
 }
 
